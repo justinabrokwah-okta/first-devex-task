41fb6ceab2ee68f8b825e2f4392e2b98d2c0f1e5
Justin Abrokwah <justin.abrokwah@okta.com>
2020-05-13 10:29:03 -0400

    Can create user in Okta organization

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ const okta = require('@okta/okta-sdk-nodejs');
 require('dotenv').config();
 
 const client = new okta.Client({
-    orgUrl: `https://${process.env.OKTA_DOMAIN}/`,
-    token: process.env.API_TOKEN // Obtained from Developer Dashboard
+    orgUrl: process.env.OKTA_DOMAIN,
+    token: process.env.API_TOKEN
 });
+
+// Verify client connection
+// console.log(client);
+
+// Create new user object and add to Okta org.
+const newUser = {
+    profile: {
+        firstName: 'Michael',
+        lastName: 'Jordan',
+        email: 'thelastdance@netflix.com',
+        login: 'thelastdance@netflix.com',
+    },
+    credentials: {
+        password: {
+            value: '6rings4CHI'
+        }
+    }
+};
+
+client.createUser(newUser)
+    .then(user => {
+        console.log('Created user', user);
+    })
+    .catch(err => console.log(err));
